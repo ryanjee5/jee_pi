2303ac9cf23115cc49dc5cb158da853a6f2ba523
ryanjee5 <rjee@terpmail.umd.edu>
2025-10-06 16:30:03 -0400

    Add files via upload

--- Lab5_1.py
+++ Lab5_1.py
@@ -8,26 +8,33 @@ import RPi.GPIO as GPIO
 import time
 import math
 GPIO.setmode(GPIO.BCM)
-p=4 #pin number
+
+pins=[4, 17, 27, 22, 5, 6, 13, 19, 26, 23] #pin number
 base_f = 500
 f= 0.2 #frequency
 
-GPIO.setup(p, GPIO.OUT)
-pwm = GPIO.PWM(p, base_f)
-pwm.start(0)
+for p in pins: #set each individualy pin as O
+	GPIO.setup(p, GPIO.OUT)
+	
+pwms = [GPIO.PWM(p, base_f) for p in pins] #creates individual O
+for pwm in pwms:
+	pwm.start(0)
+
 t0 = time.time()
 
 try:
-
-
 	while True:
 		t = time.time() - t0
-		B = math.sin(2*math.pi*f*t)**2
-		dc= B * 100 #duty cycle
-		pwm.ChangeDutyCycle(dc)
+		for i, pwm in enumerate(pwms): #index array of pins
+			phi_i =i * math.pi/9 # we have 10 leds not 12
+			B_i = math.sin(2*math.pi*f*t-phi_i)**2
+			dc_i= B_i * 100 #duty cycle
+			pwm.ChangeDutyCycle(dc_i)
 except KeyboardInterrupt:
     pass		
 
 finally:
-	pwm.stop()
+	for pwm in pwms:
+		pwm.ChangeDutyCycle(0)
+		pwm.stop()
 	GPIO.cleanup()
